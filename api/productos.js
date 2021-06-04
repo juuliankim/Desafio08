10dc0eefb6979459b776c4541461223009fb119c
julian kim <juuliank@gmail.com>
2021-06-03 22:25:18 -0300

    Desafio08

--- api/productos.js
+++ api/productos.js
@@ -1,9 +1,31 @@
+let archivo = 'productos.txt';
+const fs = require ('fs');
+
 class Productos {
     constructor() {
         // incializar variables
+        this.listaProductos=[{}];
     }
+    read(){
+        const contenido = fs.readFileSync(archivo, 'utf-8');
+        this.listaProductos=JSON.parse(contenido);
+         return JSON.parse(contenido);
+    }
+    save(objeto){
+        console.log(objeto);
+        const productos =  this.read();
 
-    // agregar los metodos requeridos
+        let id=productos.length+1;
+        let item={
+             id:id,
+             title:objeto.title,
+              price:objeto.price,
+             thumbnail:objeto.thumbnail
+        }
+        productos.push(item);
+        fs.writeFileSync(archivo,JSON.stringify(productos,null,'\t'));
+        return item;
+    }
 }
 
 // exporto una instancia de la clase
